896c38d94372343456184bb5366eab901cdb25c1
Andre <abclark@cosmicdivision.design>
2019-03-28 21:03:57 -0700

    new prod, dev config files for dev/prod build mode

--- src/webpack.prod.js
+++ src/webpack.prod.js
@@ -11,67 +11,71 @@ const imageminPngquant = require('imagemin-pngquant');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: {
-        "app": "./js/_entry.js",
-        // "app.min": "./js/_entry.js",  // no need for second min file when using production build
-    },
-    output: {
-        path: path.resolve(__dirname, '../assets'),
-        filename: 'js/[name].js',
-        publicPath: '/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env']
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader"
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader?-url",
-                    "sass-loader",
-                    'import-glob-loader',
-                ],
-            },
-            {
-                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts',    // where the fonts will go
-                        publicPath: '../fonts'  // override the default path
-                    }
-                }]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                loader: 'file-loader',
+// const merge = require('webpack-merge');
+// const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+	mode: 'production',
+	entry: {
+		"app": "./js/_entry.js",
+		// "app.min": "./js/_entry.js",  // no need for second min file when using production build
+	},
+	output: {
+		path: path.resolve(__dirname, '../assets'),
+		filename: 'js/[name].js',
+		publicPath: '/'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env']
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader"
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader?-url",
+					"sass-loader",
+					'import-glob-loader',
+				],
+			},
+			{
+				test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'fonts',    // where the fonts will go
+						publicPath: '../fonts'  // override the default path
+					}
+				}]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				loader: 'file-loader',
 				options: {
 					name: '[name].[ext]',
 					outputPath: 'img',
 					publicPath: '../img'
 				}
-            }
-        ]
-    },
+			}
+		]
+	},
     plugins: [
         new MiniCssExtractPlugin({
             filename: "css/[name].css"
