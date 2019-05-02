4291ea1bd0eb822821ebc7a9b78868f94d9ce893
Lydia Ramus <lrherring@students.nic.edu>
2019-05-02 12:31:46 -0700

    guestbook
    This is everything we had done that was saved before the great crash of May 2, 2019

--- guestbook.php
+++ guestbook.php
@@ -1,3 +1,19 @@
+<?php  include('server.php'); ?>
+
+<?php
+	if (isset($_GET['edit'])) {
+		$id = $_GET['edit'];
+		$update = true;
+		$record = mysqli_query($db, "SELECT * FROM info WHERE id=$id");
+
+		if (count($record) == 1 ) {
+			$n = mysqli_fetch_array($record);
+			$name = $n['name'];
+			$address = $n['address'];
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
