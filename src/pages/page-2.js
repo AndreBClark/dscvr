2da4c3c1eab3cb54bceae9cf896a7d1fd768e60b
Andre <andre@cosmicdivision.dev>
2019-11-14 18:37:43 -0800

    replace layout with base

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Base from "../components/Base"
 
 const SecondPage = () => (
-  <Layout>
+  <Base>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  </Base>
 )
 
 export default SecondPage
