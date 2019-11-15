8c3b7a890dc27c282e5b2139f5686e4b3ec9d9c7
Andre <andre@cosmicdivision.dev>
2019-11-14 18:37:57 -0800

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+return (
+    <footer>
+            <h4>
+                Thanks for Attending!
+            </h4>
+    </footer>
+        )
+    }
+export default Footer
